5a12bd820c7520ae7429deca69c82b6b2dfd6321
Arief <48835089+ariefnhidayah@users.noreply.github.com>
2022-03-18 15:03:57 +0700

    Update ProductController.js

--- controllers/v1/ProductController.js
+++ controllers/v1/ProductController.js
@@ -49,7 +49,7 @@ Seller.belongsTo(Province, {
 
 module.exports = {
     async get_list (req, res) {
-        let { page, limit, category, q, detail, province, city, fromLocation, address, limitDistance } = req.query
+        let { page, limit, category, q, detail, province, city, seller_id } = req.query
 
         let products
 
@@ -64,41 +64,6 @@ module.exports = {
         }
 
         let offset = (page - 1) * limit
-
-        limitDistance = limitDistance ?? 10
-
-        if (parseInt(fromLocation) === 1) {
-            const location = await HereLocation.geocode(address)
-            const {lat, lng} = location.position
-
-            let queryWhere = " WHERE `Product`.`status` = 1 AND `Product`.`name` LIKE '%" + q + "%'";
-
-            queryWhere += ` AND (
-                6971 * acos(
-                    cos(
-                        radians('${lat}')) * cos(
-                        radians( latitude )) * cos(
-                        radians( longitude ) - radians(
-                        '${lng}')) + sin(
-                        radians(
-                        '${lat}')) * sin(
-                    radians( latitude )))) <= ${limitDistance}`;
-            
-            if (category) {
-                queryWhere += " AND `Category`.`id` = '"+ category +"'"
-            }
-
-            let querySelect = "SELECT `Product`.`id`, `Product`.`id`, `Product`.`category_id`, `Product`.`seller_id`, `Product`.`name`, `Product`.`description`, `Product`.`price`, `Product`.`stock`, `Product`.`seo_url`, `Product`.`weight`, `Product`.`thumbnail`, `Product`.`created_at` AS `createdAt`, `Product`.`updated_at` AS `updatedAt`, `Category`.`id` AS `Category.id`, `Category`.`name` AS `Category.name`, `Seller`.`id` AS `Seller.id`, `Seller`.`store_name` AS `Seller.store_name`, `Seller`.`courier` AS `Seller.courier`, `Seller->City`.`id` AS `Seller.City.id`, `Seller->City`.`type` AS `Seller.City.type`, `Seller->City`.`name` AS `Seller.City.name`, `Seller->Province`.`id` AS `Seller.Province.id`, `Seller->Province`.`name` AS `Seller.Province.name`, (6971 * acos(cos(radians('" + lat + "')) * cos(radians( latitude )) * cos(radians( longitude ) - radians('" + lng + "')) + sin(radians('" + lat + "')) * sin(radians( latitude )))) AS distance FROM `products` AS `Product` INNER JOIN `categories` AS `Category` ON `Product`.`category_id` = `Category`.`id`  AND `Category`.`status` = 1 INNER JOIN `sellers` AS `Seller` ON `Product`.`seller_id` = `Seller`.`id`  AND `Seller`.`status` = 'active' LEFT OUTER JOIN `cities` AS `Seller->City` ON `Seller`.`city_id` = `Seller->City`.`id` LEFT OUTER JOIN `provincies` AS `Seller->Province` ON `Seller`.`province_id` = `Seller->Province`.`id` ";
-
-            querySelect += queryWhere + ` ORDER BY distance ASC LIMIT ${offset}, ${limit}`;
-
-            products = await sequelize.query(querySelect, {type: QueryTypes.SELECT, nest: true})
-
-            return res.json({
-                status: "testing",
-                data: products,
-            })
-        }
 
         if (city && province) {
             if (!category) {
@@ -209,13 +174,20 @@ module.exports = {
             if (!category) {
                 products = await Product.findAll({
                     where: {
-                        status: 1,
-                        name: {
-                            [Op.like]: `%${q}%`
-                        },
-                        id: {
-                            [Op.not]: detail ? detail : false
-                        }
+                        [Op.and]: [
+                            {
+                                status: 1,
+                                name: {
+                                    [Op.like]: `%${q}%`
+                                },
+                                id: {
+                                    [Op.not]: detail ? detail : false
+                                }
+                            },
+                            seller_id ? {
+                                seller_id: seller_id
+                            } : {}
+                        ]
                     },
                     offset: offset,
                     limit: Number(limit),
@@ -252,14 +224,21 @@ module.exports = {
             } else {
                 products = await Product.findAll({
                     where: {
-                        status: 1,
-                        category_id: category,
-                        name: {
-                            [Op.like]: `%${q}%`
-                        },
-                        id: {
-                            [Op.not]: detail ? detail : false
-                        }
+                        [Op.and]: [
+                            {
+                                status: 1,
+                                category_id: category,
+                                name: {
+                                    [Op.like]: `%${q}%`
+                                },
+                                id: {
+                                    [Op.not]: detail ? detail : false
+                                },
+                            },
+                            seller_id ? {
+                                seller_id: seller_id
+                            } : {}
+                        ]
                     },
                     offset: offset,
                     limit: Number(limit),
